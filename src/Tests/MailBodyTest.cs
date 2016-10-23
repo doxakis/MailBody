ea5e996a7885ab49b4c00381b97959f4870081c2
Philip Doxakis <doxakisp@gmail.com>
2016-10-23 16:31:58 -0400

    unit test

---
+++ src/Tests/MailBodyTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using MailBodyPack;
+
+namespace Tests
+{
+    public class MailBodyTest
+    {
+        [Fact]
+        public void Simple()
+        {
+            var body = MailBody
+                .CreateBody()
+                .Paragraph("Please confirm your email address by clicking the link below.")
+                .Paragraph("We may need to send you critical information about our service and it is important that we have an accurate email address.")
+                .Button("https://example.com/", "Confirm Email Address")
+                .Paragraph("— [Insert company name here]")
+                .ToString();
+
+            Assert.NotEmpty(body);
+        }
+
+        [Fact]
+        public void WithFooter()
+        {
+            var footer = MailBody
+                .CreateBlock()
+                .Text("Follow ")
+                .Link("http://twitter.com/example", "@Example")
+                .Text(" on Twitter.");
+
+            var body = MailBody
+                .CreateBody(footer)
+                .Paragraph("Please confirm your email address by clicking the link below.")
+                .Paragraph("We may need to send you critical information about our service and it is important that we have an accurate email address.")
+                .Button("https://www.example.com/", "Confirm Email Address")
+                .Paragraph("— [Insert company name here]")
+                .ToString();
+
+            Assert.NotEmpty(body);
+        }
+
+        [Fact]
+        public void CustomThemeAndRawHtml()
+        {
+            var footer = MailBody
+                .CreateBlock()
+                .Text("Follow ")
+                .Link("http://twitter.com/example", "@Example")
+                .Text(" on Twitter.");
+
+            var template = MailBodyTemplate.GetDefaultTemplate();
+            template.Paragraph = "<p style='color:blue;'>{0}</p>";
+
+            var body = MailBody
+                .CreateBody(template, footer)
+                .Paragraph("Please confirm your email address by clicking the link below.")
+                .Raw("<p>We may need to send you <strong>critical information</strong> about our service and it is important that we have an accurate email address.</p>")
+                .Button("https://www.example.com/", "Confirm Email Address")
+                .Paragraph("— [Insert company name here]")
+                .ToString();
+
+            Assert.NotEmpty(body);
+        }
+    }
+}
