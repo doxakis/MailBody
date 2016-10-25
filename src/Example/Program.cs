2bacce9ecf304cfe66befff09fb18859a193a6d4
Philip Doxakis <doxakisp@gmail.com>
2016-10-24 23:55:31 -0400

    remove capitalize on button

--- src/Example/Program.cs
+++ src/Example/Program.cs
@@ -11,22 +11,22 @@ namespace Example
     {
         public static void Main(string[] args)
         {
-            var footer = MailBody
-                .CreateBlock()
-                .Text("Follow ")
-                .Link("http://twitter.com/example", "@Example")
-                .Text(" on Twitter.");
+            //var footer = MailBody
+            //    .CreateBlock()
+            //    .Text("Follow ")
+            //    .Link("http://twitter.com/example", "@Example")
+            //    .Text(" on Twitter.");
 
-            var template = MailBodyTemplate.GetDefaultTemplate();
-            template.Paragraph = "<p style='color:blue;'>{0}</p>";
+            //var template = MailBodyTemplate.GetDefaultTemplate();
+            //template.Paragraph = "<p style='color:blue;'>{0}</p>";
 
-            var body = MailBody
-                .CreateBody(template, footer)
-                .Paragraph("Please confirm your email address by clicking the link below.")
-                .Raw("<p>We may need to send you <strong>critical information</strong> about our service and it is important that we have an accurate email address.</p>")
-                .Button("https://www.example.com/", "Confirm Email Address")
-                .Paragraph("— [Insert company name here]")
-                .ToString();
+            //var body = MailBody
+            //    .CreateBody(template, footer)
+            //    .Paragraph("Please confirm your email address by clicking the link below.")
+            //    .Raw("<p>We may need to send you <strong>critical information</strong> about our service and it is important that we have an accurate email address.</p>")
+            //    .Button("https://www.example.com/", "Confirm Email Address")
+            //    .Paragraph("— [Insert company name here]")
+            //    .ToString();
 
             //var body = MailBody
             //    .CreateBody()
@@ -37,7 +37,7 @@ namespace Example
             //    .ToString();
 
             // Save on disk (for testing)
-            File.WriteAllText(@"C:\temp\index.html", body);
+            //File.WriteAllText(@"C:\temp\index.html", body);
         }
     }
 }
